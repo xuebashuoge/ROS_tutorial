88463f9e7cbde1acf7217f167cfa2acbd4131a58
xuebashuoge <1172508020@qq.com>
2020-03-01 07:52:50 +0800

    first commit

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/hys/Desktop/vscode/ros/catkin_ws/src
+# Build directory: /home/hys/vscode/ros/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(begin)
+subdirs(ros_service_examples)
